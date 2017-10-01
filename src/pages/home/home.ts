3a910ce42499bee0446254c9b20cf3e9d50540d9
Sean Hill <sean.daryl.hill@gmail.com>
2017-10-01 11:11:51 -0700

    ion2-calendar prod bug example

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,33 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component } from '@angular/core'
+import { NavController, ModalController } from 'ionic-angular'
+import { CalendarModal, CalendarModalOptions } from 'ion2-calendar'
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  constructor(
+    public navCtrl: NavController,
+    public modalCtrl: ModalController
+  ) {}
 
-  constructor(public navCtrl: NavController) {
+  openCalendar() {
+    const options: CalendarModalOptions = {
+      pickMode: 'range',
+      title: 'Select dates',
+      closeIcon: true,
+      doneLabel: 'Save'
+    }
 
+    const datePicker = this.modalCtrl.create(CalendarModal, {
+      options: options
+    })
+
+    datePicker.present()
+
+    datePicker.onDidDismiss(date => {
+      console.log(date)
+    })
   }
-
 }
